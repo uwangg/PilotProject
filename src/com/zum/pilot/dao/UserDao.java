3151b07591588eab39b541c97c1b2d95b65eff83
DESKTOP-PJGF47R\김은진 <uwangg@gmail.com>
2017-04-21 13:55:14 +0900

    [#70469] JdbcTemplate을 이용해 UserDao 리팩토링 (delete부분 제외)
    delete 부분은 트랜잭션 처리 후 리팩토링 적용 예정

--- src/com/zum/pilot/dao/UserDao.java
+++ src/com/zum/pilot/dao/UserDao.java
@@ -21,53 +21,40 @@ public class UserDao {
 	
 	// id를 이용해 user 정보 가져오기
 	public UserVo get(Long number) {
-
-		UserVo userVo = null;
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			con = dbConnection.getConnection();
-			String query = "select id, email, name from user where no=? and delete_flag=0";
-			pstmt = con.prepareStatement(query);
+		JdbcTemplate template = new JdbcTemplate();
+		String query = "select id, email, name from user where no=? and delete_flag=0";
+		PreparedStatementSetter pss = new PreparedStatementSetter() {
 			
-			pstmt.setLong(1, number);	// 첫번째  ?에 id값
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				Long id = rs.getLong("id");
-				String email = rs.getString("email");
-				String name = rs.getString("name");
-				
-				userVo = new UserVo();
-				userVo.setId(id);
-				userVo.setEmail(email);
-				userVo.setName(name);
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {
+				pstmt.setLong(1, number);	// 첫번째  ?에 id값
 			}
+		};
+		RowMapper rm = new RowMapper() {
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(con != null)
-					con.close();
-				if(pstmt != null)
-					pstmt.close();
-				if(rs != null)
-					rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			@Override
+			public Object mapRow(ResultSet rs) throws SQLException {
+				UserVo userVo = null;
+				while(rs.next()) {
+					Long id = rs.getLong("id");
+					String email = rs.getString("email");
+					String name = rs.getString("name");
+					
+					userVo = new UserVo();
+					userVo.setId(id);
+					userVo.setEmail(email);
+					userVo.setName(name);
+				}
+				return userVo;
 			}
-		}
-		
-		return userVo;
+		};
+		return (UserVo)template.executeQuery(query, pss, rm);
 	}
 	
 	// 회원인증시
 	public UserVo get(UserVo vo) {		
+		JdbcTemplate template = new JdbcTemplate();
+		String query = "select id, email, name from user where email=? and passwd=? and delete_flag=0";
 		PreparedStatementSetter pss = new PreparedStatementSetter() {
 			
 			@Override
@@ -94,121 +81,84 @@ public class UserDao {
 				return userVo;
 			}
 		};
-		JdbcTemplate template = new JdbcTemplate() {
-		};
-		String query = "select id, email, name from user where email=? and passwd=? and delete_flag=0";
 		return (UserVo)template.executeQuery(query, pss, rm);
 	}
 	
 	// 회원가입시 중복성체크
+	// 닉네임 중복체크
 	public boolean checkName(String name) {
-		boolean check = true;	// name이 존재하면 true
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			con = dbConnection.getConnection();
-			String query = "select count(*) from user where name=? and delete_flag=0";
-			pstmt = con.prepareStatement(query);
+		JdbcTemplate template = new JdbcTemplate();
+		String query = "select count(*) from user where name=? and delete_flag=0";
+		PreparedStatementSetter pss = new PreparedStatementSetter() {
 			
-			pstmt.setString(1, name);	// 첫번째  ?에 id값
-			rs = pstmt.executeQuery();
-			rs.next();
-			if(rs.getInt(1) == 0)
-				check = false;
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(con != null)
-					con.close();
-				if(pstmt != null)
-					pstmt.close();
-				if(rs != null)
-					rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {				
+				pstmt.setString(1, name);	// 첫번째  ?에 id값
 			}
-		}
-		
-		return check;
+		};
+		RowMapper rm = new RowMapper() {
+			
+			@Override
+			public Object mapRow(ResultSet rs) throws SQLException {
+				boolean check = true;	// name이 존재하면 true
+				rs.next();
+				if(rs.getInt(1) == 0)
+					check = false;
+				return check;
+			}
+		};
+		return (boolean)template.executeQuery(query, pss, rm);
 	}
+	
+	// 이메일 중복체크
 	public boolean checkEmail(String email) {
-		boolean check = true;	// name이 존재하면 true
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			con = dbConnection.getConnection();
-			String query = "select count(*) from user where email=? and delete_flag=0";
-			pstmt = con.prepareStatement(query);
+		JdbcTemplate template = new JdbcTemplate();
+		String query = "select count(*) from user where email=? and delete_flag=0";
+		PreparedStatementSetter pss = new PreparedStatementSetter() {
 			
-			pstmt.setString(1, email);	// 첫번째  ?에 id값
-			rs = pstmt.executeQuery();
-			rs.next();
-			if(rs.getInt(1) == 0)
-				check = false;
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(con != null)
-					con.close();
-				if(pstmt != null)
-					pstmt.close();
-				if(rs != null)
-					rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, email);	// 첫번째  ?에 id값
 			}
-		}
-		
-		return check;
+		};
+		RowMapper rm = new RowMapper() {
+			
+			@Override
+			public Object mapRow(ResultSet rs) throws SQLException {
+				boolean check = true;	// name이 존재하면 true
+				rs.next();
+				if(rs.getInt(1) == 0)
+					check = false;
+				return check;
+			}
+		};
+		return (Boolean)template.executeQuery(query, pss, rm);
 	}
+	
+	// 패스워드 체크
 	public boolean checkPassword(Long id, String password) {
-		boolean check = true;	// name이 존재하면 true
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			con = dbConnection.getConnection();
-			String query = "select count(*) from user where id=? and passwd=? and delete_flag=0";
-			pstmt = con.prepareStatement(query);
+		JdbcTemplate template = new JdbcTemplate();
+		String query = "select count(*) from user where id=? and passwd=? and delete_flag=0";
+		PreparedStatementSetter pss = new PreparedStatementSetter() {
 			
-			pstmt.setLong(1, id);	// 첫번째  ?에 id값
-			pstmt.setString(2, password);
-			rs = pstmt.executeQuery();
-			rs.next();
-			if(rs.getInt(1) == 0)
-				check = false;
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(con != null)
-					con.close();
-				if(pstmt != null)
-					pstmt.close();
-				if(rs != null)
-					rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {				
+				pstmt.setLong(1, id);	// 첫번째  ?에 id값
+				pstmt.setString(2, password);
 			}
-		}
-		
-		return check;
+		};
+		RowMapper rm = new RowMapper() {
+			
+			@Override
+			public Object mapRow(ResultSet rs) throws SQLException {
+				boolean check = true;	// name이 존재하면 true
+				rs.next();
+				if(rs.getInt(1) == 0)
+					check = false;
+				return check;
+			}
+		};
+		return (boolean)template.executeQuery(query, pss, rm);
 	}
 	
 	// 회원가입시
