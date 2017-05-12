0ca89563b9d0f1d79a879ce5b30b88ebad63a87a
DESKTOP-PJGF47R\김은진 <uwangg@gmail.com>
2017-05-12 17:23:17 +0900

    [#70730] MainController 부분 servlet에서 spring으로 변경
    기존의 MainController를 MainController2로 이동
    MainController를 spring코드로 변경
    @WebServlet("/") -> @RequestMapping("/")
    main/DefaultAction -> MainController에서 main 메소드로 변경
    request.getParameter -> @RequestParam
    request.setAttribute -> model.addAttribute

--- src/main/java/com/zum/pilot/controller/MainController.java
+++ src/main/java/com/zum/pilot/controller/MainController.java
@@ -1,37 +1,51 @@
 package com.zum.pilot.controller;
 
-import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.zum.pilot.dao.PostDao;
+import com.zum.pilot.vo.PostVo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.zum.pilot.action.Action;
-import com.zum.pilot.action.ActionFactory;
-import com.zum.pilot.action.main.MainActionFactory;
+import java.util.List;
 
-@WebServlet("/")
-public class MainController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller
+public class MainController {
 
-  public MainController() {
-    super();
-  }
+  @RequestMapping("/")
+  public String main(
+          @RequestParam(value = "currentPageNum", defaultValue = "1") int currentPageNum, // 선택된 페이지 번호
+          @RequestParam(value = "begin", defaultValue = "1") int begin,
+          @RequestParam(value = "end", defaultValue = "1") int end,
+          Model model) {
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    doAction(request, response);
-  }
+    // 게시글 페이지네이션
+    Long totalPostNum = 0L;    // 게시글의 총 갯수
+    int totalPageNum = 0;    // 총 페이지 번호의 수
+    int postUnit = 10;    // 한 페이지당 보여줄 글의 최대 갯수
+    int pageNumUnit = 5;    // 한 페이지 블락당 보여줄 번호의 최대 갯수
 
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    doAction(request, response);
-  }
+    List<PostVo> postList = null;
 
-  protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String actionName = request.getParameter("action");
-    ActionFactory actionFactory = MainActionFactory.INSTANCE;
-    Action action = actionFactory.getAction(actionName);
-    action.execute(request, response);
+    PostDao postDao = PostDao.INSTANCE;
+
+    totalPostNum = postDao.totalNumberOfPost();
+    totalPageNum = (int) ((totalPostNum - 1) / postUnit + 1);
+
+    postList = postDao.getList((totalPageNum - currentPageNum) * postUnit, postUnit);    // 선택된 페이지번호, 보여줄 글의갯수
+
+    end = (begin - 1) + pageNumUnit;
+    if (end > totalPageNum)
+      end = totalPageNum;
+
+    model.addAttribute("begin", begin);
+    model.addAttribute("end", end);
+    model.addAttribute("totalPageNum", totalPageNum);
+    model.addAttribute("currentPageNum", currentPageNum);
+    model.addAttribute("pageNumUnit", pageNumUnit);
+    model.addAttribute("postList", postList);
+
+    return "forward:/WEB-INF/views/main/index.jsp";
   }
 }
