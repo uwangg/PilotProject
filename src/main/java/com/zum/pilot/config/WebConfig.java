17c74a7f0ef4a933a43f0eac60029bd6d453559d
DESKTOP-PJGF47R\김은진 <uwangg@gmail.com>
2017-06-02 13:25:27 +0900

    [#71044] Spring multipartrequest 디펜던시 추가, 리졸버 추가

--- src/main/java/com/zum/pilot/config/WebConfig.java
+++ src/main/java/com/zum/pilot/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.zum.pilot.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -27,5 +28,19 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     viewResolver.setPrefix("/WEB-INF/views/");
     viewResolver.setSuffix(".jsp");
     return viewResolver;
+  }
+
+  // MultipartResolver
+  @Bean
+  public CommonsMultipartResolver multipartResolver() {
+    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+    multipartResolver.setMaxUploadSize(10485760); // 파일 업로드 용량
+    return multipartResolver;
+  }
+
+  // 파일 업로드 경로
+  @Bean
+  public String uploadPath() {
+    return "D:\\test\\upload";
   }
 }
